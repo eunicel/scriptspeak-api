e7bb6615e31f4974cc617d9f832526163bd074df
Kelly Liu <kelyliu@mit.edu>
2014-10-31 14:21:48 -0400

    created phrases routes/api methods

--- models/phrase.js
+++ models/phrase.js
@@ -3,5 +3,59 @@ var moment = require('moment');
 
 var phraseSchema = mongoose.Schema({
 	text: {type: String, required: true},
-	date: {type: Date, default: moment()}
+	dateCreated: {type: Date, default: moment()},
+	numTimesUsed: {type: Number},
+	author: {type: objectId, ref: 'User', required: true},
 });
+
+phraseSchema.method('createPhrase', function(text, dateCreated, numTimesUsed, callback) {
+	var phrase = new Phrase ({
+		text:text,
+		dateCreated: dateCreated,
+		numTimesUsed: numTimesUsed,
+		author
+	});
+
+	phrase.save(function(err) {
+		callback(err, phrase);
+	});
+});
+
+phraseSchema.method('sortByDateCreated', function(phrases, callback) {
+	if (phrases != null) {
+		phrases.sort({dateCreated: -1});
+		callback(phrases);
+	}
+	
+});
+
+phraseSchema.method('sortByNumTimesUsed', function(phrases, callback) {
+	if (phrases != null) {
+		phrases.sort({numTimesUsed: -1});
+		callback(phrases);
+	}
+	
+});
+
+phraseSchema.method('deletePhrase', function(phraseId, author, callback) {
+	Phrase.findOneAndRemove({_id: phraseId, author: author}, function(err, phrase) {
+		callback(err, phrase);
+	});
+});
+
+phraseSchema.method('getAll', function(callback) {
+	Phrase.find({}, function(err, phrases) {
+		callback(err, phrases);
+	});
+	
+});
+
+phraseSchema.method('getById', function(phraseId, callback) {
+	Phrase.findById(phraseId, function(err, found) {
+		callback(err, found)
+	});
+
+})
+var Phrase = mongoose.model('Phrase', phraseSchema);
+
+module.exports = Phrase;
